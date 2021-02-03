e9308ca75db31fa01e0065e3906a28cf0aac071c
Marcos Fillipe <53455496+marcosfillipe@users.noreply.github.com>
2021-02-03 16:43:11 -0300

    Em andamento Aula 2

--- backend-server/src/server.ts
+++ backend-server/src/server.ts
@@ -1,19 +1,64 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express();
 
+app.use(express.json());
+
+
+//Rota: Endereço completo da requisição
+//Recurso: Qual entidade estamos acessando do Sistema
+
+//GET: Buscar uma ou mais informações do back-end
+//POST: Criar uma nova informação do back-end
+//PUT: Atualizar uma informação existente no back-end
+//DELETE: Remover uma informação do back-end
+
+
+//Request Param: Parâmetros que vem na própria rota que identificam um recurso
+//Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+//Request Body: Parâmetros para criação/atualização de informações
+
+
+const users = [
+    'Marcos',
+    'Gansola',
+    'Yamete',
+    'Rafael',
+    'Negão'
+    
+];
+
 app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+    const search = String(request.query.search);
 
-    response.json([
-        'Marcos',
-        'Gansola',
-        'Yamete',
-        'Rafael',
-        'Negão'
-    ]);
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
 
+    return response.json(filteredUsers);
 });
+
+
+app.get('/users/:id', (request, response) => {
+
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) =>{
+
+const data = request.body;
+
+const user = {
+     name: data.name,
+     email: data.email,
+ };
+
+   return response.json(user);
+
+})
+
 
 app.listen(3333);
 
