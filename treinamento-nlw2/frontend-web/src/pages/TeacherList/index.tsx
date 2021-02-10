2fa9481a4de19b31b4b70ebe71ae81a23d3c446c
Marcos Fillipe <53455496+marcosfillipe@users.noreply.github.com>
2021-02-10 12:10:16 -0300

    Aula 1 Em andamento parte 2

--- treinamento-nlw2/frontend-web/src/pages/TeacherList/index.tsx
+++ treinamento-nlw2/frontend-web/src/pages/TeacherList/index.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar" />
-          </Link>
-          <img src={logoImg} alt="Proffy" />
-        </div>
+      <PageHeader title="Estes são os proffys disponiveis">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Materia</label>
+                        <input type="text" id="subject"/>
+                    </div>
+                    <div className="input-block">
 
-        <div className="header-content">
-          <strong></strong>
-        </div>
-
-      </header>
+                        <label htmlFor="week_day">Dia da Semana</label>
+                        <input type="text" id="week_day"/>
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time"/>
+                    </div>
+                    
+                </form>
+            </PageHeader>
     </div>
   );
 }
