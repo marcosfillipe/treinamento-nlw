42fdc54af2f789430d46121db3f8cc90a08c007b
Marcos Fillipe <marcosfillipebezerra@gmail.com>
2021-02-14 22:11:17 -0300

    Iniciando Aulas de Mobile

---
+++ treinamento-nlw2/mobile-app/src/pages/Landing/index.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+
+function Landing() {
+  return (
+    <View styles= { styles.container } >
+    <Image source={ LandingImg } />
+      < /View>
+  )
+}
+
+export default Landing;
