4a34cb377d6c3e845079f13a3842753783940ad8
Marcos Fillipe <53455496+marcosfillipe@users.noreply.github.com>
2021-02-15 16:23:48 -0300

    Iniciando a aula 1 NLW3

---
+++ treinamento-nlw3/frontend-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/global.css'
+
+function App() {
+  return (
+    <div id="page-landing">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
