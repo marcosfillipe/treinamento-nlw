92766028a06f6c89b7ac9306f9c15db5ae3f98a8
Marcos Fillipe <marcosfillipebezerra@gmail.com>
2021-02-19 01:47:19 -0300

    Aula 2 Finalizada

--- treinamento-nlw3/backend-server/src/server.ts
+++ treinamento-nlw3/backend-server/src/server.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import path from 'path';
+import 'express-async-errors';
+import cors from 'cors';
+
 import './database/connection';
+
 import routes from './routes';
+import errorHandler from './errors/handles';
+
 
 
 const app = express();
 
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 
 
-   
 
 app.listen(3333);
