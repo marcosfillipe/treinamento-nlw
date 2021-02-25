572ee07790cce2e0a7395e3fb1c238146fdc01dc
Marcos Fillipe <53455496+marcosfillipe@users.noreply.github.com>
2021-02-25 16:49:27 -0300

    Aula 3 em andamento..

--- treinamento-nlw4/moveit-next/src/components/Countdown.tsx
+++ treinamento-nlw4/moveit-next/src/components/Countdown.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/components/Countdown.module.css';
 export function Countdown() {
 
   const [time, setTime] = useState(25 * 60);
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -13,17 +13,17 @@ export function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
   function startCountdown() {
-    setActive(true);
+    setIsActive(true);
   }
 
 
   useEffect(() => {
-    if (active && time > 0) {
+    if (isActive && time > 0) {
       setTimeout(() => {
         setTime(time - 1);
       }, 1000)
     }
-  }, [active, time])
+  }, [isActive, time])
 
 
 
@@ -43,13 +43,14 @@ export function Countdown() {
       </div>
 
       <button
-        type="button"
-        className={styles.countdownButton}
-        onClick={startCountdown}
-      >
-        Iniciar um Ciclo
-      </button>
+          type="button"
+          className={styles.countdownButton}
+          onClick={startCountdown}
 
+      >
+        Teste
+          
+        </button>
     </div>
   );
 }
